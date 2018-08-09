6f4fe1ee2a0bfc187564e076d2c2f173a01f63a2
dmpsyjsn <jdempsey@gopmo.com>
2018-08-08 19:40:09 -0700

    Added my first unit (or possibly integration) test, using Moq.

---
+++ source/Tests/IntegrationTesting/Games/GamesTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VideoGameHash.Handlers.Games.Commands;
+using VideoGameHash.Handlers.Games.Queries;
+using VideoGameHash.Messages.Games.Commands;
+using VideoGameHash.Messages.Games.Queries;
+using VideoGameHash.Models;
+using VideoGameHash.Models.TheGamesDB;
+
+namespace Tests.IntegrationTesting.Games
+{
+    [TestClass]
+    public class GamesTests
+    {
+        [TestMethod]
+        public async Task AddGame_Succeeds()
+        {
+            var mockGamesSet = MockHelpers.CreateMockSet<VideoGameHash.Models.Games>();
+            var mockGameIgnoresSet = MockHelpers.CreateMockSet<GameIgnore>();
+ 
+            var mockContext = new Mock<VGHDatabaseContainer>(); 
+            mockContext.Setup(m => m.Games).Returns(mockGamesSet.Object);
+            mockContext.Setup(m => m.GameIgnores).Returns(mockGameIgnoresSet.Object);
+            
+            var queryHandler = new GetGamesHandler(mockContext.Object);
+
+            var result = (await queryHandler.Handle(new GetGames())).ToList();
+
+            Assert.IsTrue(result.Count == 0);
+
+            var handler = new AddGamesHandler(mockContext.Object);
+
+            var game = new Game
+            {
+                game_title = "Test",
+                release_date = "2018-08-08"
+            };
+
+            var command = new AddGames
+            {
+                Games = new List<Game> {game}
+            };
+
+            await handler.Handle(command);
+
+            result = (await queryHandler.Handle(new GetGames())).ToList();
+
+            Assert.IsTrue(result.Count == 1 && result[0].GameTitle.Equals(game.game_title));
+        }
+    }
+}
---
+++ source/Tests/MockHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using Moq;
+
+namespace Tests
+{
+    public static class MockHelpers
+    {
+        public static Mock<DbSet<T>> CreateMockSet<T>() where T : class
+        {
+            var mockSet = new Mock<DbSet<T>>(); 
+
+            var sourceList = new List<T>();
+            var data = sourceList.AsQueryable();
+
+            mockSet.As<IDbAsyncEnumerable<T>>() 
+                .Setup(m => m.GetAsyncEnumerator()) 
+                .Returns(new TestDbAsyncEnumerator<T>(data.GetEnumerator())); 
+ 
+            mockSet.As<IQueryable<T>>() 
+                .Setup(m => m.Provider) 
+                .Returns(new TestDbAsyncQueryProvider<T>(data.Provider)); 
+ 
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression); 
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType); 
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mockSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>(s => sourceList.Add(s));
+            return mockSet;
+        }
+    }
+}
